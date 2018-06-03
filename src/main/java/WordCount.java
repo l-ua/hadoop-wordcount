b39188730e651df9fff5399bb1d5000fd474f301
1075823021@qq.com <1075823021@qq.com>
2018-06-03 16:37:20 +0800

    Initial commit

---
+++ src/main/java/WordCount.java
@@ -0,0 +1,77 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class WordCount {
+    public static class CountMap extends Mapper<LongWritable, Text, Text, IntWritable> {
+        private final IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            String line = value.toString();
+            StringTokenizer token = new StringTokenizer(line);
+            while (token.hasMoreTokens()) {
+                word.set(token.nextToken());
+                context.write(word, one);
+            }
+        }
+    }
+
+    public static class CountReduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+        public void reduce(Text key, Iterable<IntWritable> values, Context context)
+                throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            context.write(key, new IntWritable(sum));
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        String input = "hdfs://192.168.62.100:9000/hadoop-input";
+        String output = "hdfs://192.168.62.100:9000/hadoop-output";
+        Configuration conf = new Configuration();
+        conf.addResource("classpath:/hadoop/core-site.xml");
+        conf.addResource("classpath:/hadoop/hdfs-site.xml");
+        conf.addResource("classpath:/hadoop/mapred-site.xml");
+        conf.addResource("classpath:/hadoop/mapred-site.xml");
+        Job job = Job.getInstance(conf);
+        conf.set("mapreduce.framework.name", "yarn");
+        conf.set("fs.default.name", "hdfs://192.168.62.100:9000");
+        conf.set("mapred.jar","D:\\java\\proj\\hadoop-wordcount\\target\\hadoop-wordcount-1.0.0.jar");
+        conf.set("mapreduce.app-submission.cross-platform", "true");
+        System.setProperty("HADOOP_USER_NAME", "hadoop");
+        //System.setProperty("hadoop.home.dir", "D:/Develop/hadoop-2.8.0/");
+        //System.setProperty("hadoop.tmp.dir", "/home/hadoop/tmp");
+        job.setJarByClass(WordCount.class);
+        job.setJobName("wordcount");
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        job.setMapperClass(CountMap.class);
+        job.setReducerClass(CountReduce.class);
+
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        FileInputFormat.addInputPath(job, new Path(input));
+        FileOutputFormat.setOutputPath(job, new Path(output));
+
+        job.waitForCompletion(true);
+    }
+}
+
